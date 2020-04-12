5c6f084a07b00a95e8a8edc7a02f8036fec7f6a6
vintieu <kebintieu@gmail.com>
2020-04-12 22:02:43 +0200

    added big spaceship sprite as well as bouncing it up and down

--- spaceview/SpaceView.gd
+++ spaceview/SpaceView.gd
@@ -7,9 +7,15 @@ var distance = 0
 var sceen_width_minus_ship_length = 300
 var pixel_speed_per_frame = 0.04
 var dict = {}
+var ship_y_position = 0
+var ship_moves_up = true
 # Called when the node enters the scene tree for the first time.
 var mission_data
+var mission_text = ""
 func _ready():
+	get_node("TextBox").modulate = Color(1, 1, 1, 0.5)
+	
+	
 	var mission_data_file = File.new()
 	mission_data_file.open("res://invisibleData/mission_dialogue.json", File.READ)
 	var mission_data_json = JSON.parse(mission_data_file.get_as_text())
@@ -19,6 +25,7 @@ func _ready():
 	randomize()
 	var mission = generate_mission()
 	print(mission.MissionName)	
+	mission_text = mission.Text
 	var answers = generate_answers(mission)
 	for answer in answers:
 		print(answer.Text)
@@ -60,12 +67,21 @@ func generate_answers(mission):
 	
 	
 func show_mission():	
-	pass
+	get_node("MissionTextBox").text = mission_text
 
 func _process(delta):
 	if(get_node("Position2D").position.x<sceen_width_minus_ship_length):
 		get_node("Position2D").position.x =get_node("Position2D").position.x+ pixel_speed_per_frame
-
-
+	
+	
+	if(ship_y_position>6):
+		ship_moves_up = false
+	if(ship_y_position < -6):
+		ship_moves_up = true
+	if(ship_moves_up):
+		ship_y_position = ship_y_position +0.1
+	else: ship_y_position = ship_y_position -0.1
+	get_node("PositionOfShip").position.y  = ship_y_position
+	
 
 
