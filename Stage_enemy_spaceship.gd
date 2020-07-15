e16e7ddae19c1d5133c1093b4607e115087c4523
vintieu <kebintieu@gmail.com>
2020-07-15 14:54:27 +0200

    added a room generation to the bsp style

--- Stage_enemy_spaceship.gd
+++ Stage_enemy_spaceship.gd
@@ -16,25 +16,33 @@ func _ready():
 	for i in leaves:
 		print(i.center)
 	draw_on_matrix(leaves)	
-	test_print(matrix)
+	#test_print(matrix)
 	get_node("TileMapSpaceShip").draw_map(matrix)	
 
 func empty_matrix():
-	for x in range(world_size.x):
+	for x in range(world_size.x+1): #+1 to draw a last wall on the border
 			matrix.append([])
-			for y in range(world_size.y):
+			for y in range(world_size.y+1):
 				matrix[x].append(0)
 
-func draw_on_matrix(vector2_list):
-	for x in range(world_size.x):
-			for y in range(world_size.y):
-				for i in vector2_list:
-					if(i.center.x == x and i.center.y == y):
-						matrix[x][y]=1
-		
-# Called every frame. 'delta' is the elapsed time since the previous frame.
-#func _process(delta):
-#	pass
+func draw_on_matrix(leaf_list):
+				for leaf in leaf_list:
+					draw_room(leaf)
+
+func draw_room(leaf):
+
+	var size_x = leaf.size.x
+	var size_y = leaf.size.y
+	
+	for x in size_x+1: #+1 because this makes 
+		for y in size_y+1: #it possible to draw beyond the world restraints(walls)
+			matrix[x-leaf.size.x/2+leaf.center.x][y- leaf.size.y/2+leaf.center.y] = 2
+			if x == 0 or x == leaf.size.x:
+				matrix[x-leaf.size.x/2+leaf.center.x][y- leaf.size.y/2+leaf.center.y] = 1
+			if y == 0 or y == leaf.size.y:
+				matrix[x-leaf.size.x/2+leaf.center.x][y- leaf.size.y/2+leaf.center.y] = 1	
+		#matrix[leaf.center.x][leaf.center.y] = 0		
+
 func test_print(matrix):
 	for row in range(len(matrix)):
 		var current_row = ""
--- TileMapSpaceShip.gd
+++ TileMapSpaceShip.gd
@@ -13,10 +13,10 @@ func draw_map(matrix):
 		for y in len(matrix[x]):
 			if(matrix[x][y]==1):
 				set_cell(x,y, 3)
-			print("tile set")
 			if(matrix[x][y] == 2):
-				var node = slime.instance()
-				node.global_position = (Vector2( x * 16 + 8,y * 16 + 8))
-				get_parent().add_child(node)
+				set_cell(x,y, 5)
+				#var node = slime.instance()
+				#node.global_position = (Vector2( x * 16 + 8,y * 16 + 8))
+				#get_parent().add_child(node)
 	update_bitmask_region(Vector2(0,0),Vector2(164,164))#sync this			
 	
